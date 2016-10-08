0e11bb98d3f3f50f61d408fd13259ece3c01ed6e
vishalsharmas <vishalsharma11123@gmail.com>
2016-10-08 20:08:56 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,21 @@ button.onclick = function () {
       request.open('GET', 'http://vishalsharmas.imad.hasura-app.io/counter', true);
       request.send(null);
 }; 
+  //submit name
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  var submit = document.getElementById('submit_btn');
+  submit.onclick =  function () {
+      
+      //make a request to the server and send the name
+      
+      
+      //capture a list of names and render it as a list
+      var names = ['name1','name2','name3','name4'];
+      var list  = '';
+      for (var i=0; i<names.length; i++) {
+           list += '<li>' + names[i] + '<i>';
+      } 
+      var ul = document.getElementById('namelist');
+       ul.innerHTML = list;
+  };
