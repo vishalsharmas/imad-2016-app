3eb93ba75981112d46812df0f901f5f79c191195
vishalsharmas <vishalsharma11123@gmail.com>
2016-11-09 22:16:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('test-db', function (req, res)
+  {
+      
+      
+  })
+
 var counter = 0;
 app.get('/counter',function(req, res) {
     counter = counter + 1;
