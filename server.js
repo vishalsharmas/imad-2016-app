c941be18debda11647872a5544cd77212eafa0f0
vishalsharmas <vishalsharma11123@gmail.com>
2016-11-10 00:35:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+        user:'coco98',
+        database:'coco98',
+        host:'db.imad.hasura-app.io',
+        port:'5432',
+        password: process.env.DB_PASSWORD
+    
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -81,12 +92,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
 
 app.get('test-db', function (req, res)
   {
+     pool.query('SELECT * FROM test',function (err,result)  {
+         
+       if err {
+           res.status(500).send(err.tostring());
+       } else {
+          res.send(JSON.stringify(result));
+       }
+     })
       
-      
-  })
+  });
 
 var counter = 0;
 app.get('/counter',function(req, res) {
