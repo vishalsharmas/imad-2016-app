3d147e7e878dad64e608093cef338ada62ca4b90
vishalsharmas <vishalsharma11123@gmail.com>
2016-11-10 23:40:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,20 +92,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
 
-app.get('test-db', function (req, res)
-  {
-     pool.query('SELECT * FROM test',function (err,result)  {
-         
-         if err  {
-           res.status(500).send(err.tostring());
-           }   else   {
-          res.send(JSON.stringify(result));
-       }
-     });
-      
-  });
 
 var counter = 0;
 app.get('/counter',function(req, res) {
